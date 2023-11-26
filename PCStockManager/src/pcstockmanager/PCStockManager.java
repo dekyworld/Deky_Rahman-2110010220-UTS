91837a1f5d41761fbda62d755971a18f28b1c7d4
dekyworld <103550857+dekyworld@users.noreply.github.com>
2023-11-26 18:30:43 +0800

    Add files via upload

---
+++ PCStockManager/src/pcstockmanager/PCStockManager.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pcstockmanager;
+
+/**
+ *
+ * @author ASPIRE Swift 3
+ */
+public class PCStockManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
